e89ad3a5152c0809037c94baa7d8e3081433eb8c
HectorVillavicencio <103118349+HectorVillavicencio@users.noreply.github.com>
2023-03-03 17:07:30 -0300

    agregue funciones en js

--- js/function.js
+++ js/function.js
@@ -1,5 +1,17 @@
+console.log("esta conectado el js con el html");
 
-document.getElementById('fBoton').onClick=function(){
-    console.log("orubita");
-    document.getElementById("fBoton").innerHTML =location.href='https://www.facebook.com';
+document.getElementById('boton').onClick= function () {
+    console.log("funciona el maldito boton");
+} 
+
+function facebook(){
+    location.href='https://www.facebook.com';
+}
+
+function instagram(){
+    location.href='https://www.instagram.com';
+}
+
+function github(){
+    location.href='https://www.github.com';
 }
